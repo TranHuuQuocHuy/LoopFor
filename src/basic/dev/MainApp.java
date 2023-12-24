55e3ed8449e22f1d14d38977c40c601669e85e5a
quochuy232005 <144077996+TranHuuQuocHuy@users.noreply.github.com>
2023-12-24 16:09:49 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,13 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tong = 0;
+		for (int i = 0; i <= 100; i++) {
+			tong += i;
+		}
+		System.out.println("tong =" + tong);
+	}
+}
---
+++ src/basic/dev/MainApp2.java
@@ -0,0 +1,20 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhap so nguyen n: ");
+		int n = sc.nextInt();
+		int sum = 0;
+		for (int i = 1; i < n; i++) {
+			if (i % 3 == 0) {
+				sum += i;
+			}
+		}
+		System.out.println("Tong cac so chia het cho 3 va nho hon " + n + " là: " + sum);
+	}
+}
